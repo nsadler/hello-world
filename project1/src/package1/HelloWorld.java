da1a62ae0b7b6b1a37b5051bad13182d4fc6c8ec
DESKTOP\nsadler <nikolas_sadler@nidaansystems.com>
2018-05-17 13:16:59 -0700

    Update to project1

--- project1/src/package1/HelloWorld.java
+++ project1/src/package1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("First Change");
+		System.out.println("Second Change");
 	}
 }
